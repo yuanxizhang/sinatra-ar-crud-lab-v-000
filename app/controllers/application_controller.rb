7d0917d2fbf3e7f34971ba89b24962ac99c8326d
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-30 22:55:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/' do 
+      
+  end
+  
+  # "INDEX", part of "R" in CRUD
+  get '/articles' do
+  	@articles = Article.all
+  	erb :index
+  end
+
+  # "NEW", part of the C in CRUD
+  get '/article/new' do 
+  	erb :new 
+  end 
+
+  # "CREATE", part of the C in CRUD
+  post '/articles' do 
+  	@article = Article.create(title: params[:title], content: params[:content])	
+  	redirect to '/articles/@article.id'
+  end
+
+  # "SHOW", part of the R in CRUD
+  get '/articles/:id' do
+  	@article = Article.find(params[:id])
+  	erb :show
+  end
+
+  # "EDIT", part of the U in CRUD
+  get '/articles/:id/edit' do 
+  	@article = Article.find(params[:id])
+  	erb :edit 
+  end
+
+  # "UPDATE", part of the U in CRUD
+  patch '/articles/:id' do 
+  	@article = Article.find(params[:id])
+  	@article.title = params[:title]
+  	@article.content = params[:content]
+  	@article.save
+    redirect "/articles/#{@article.id}"
+  end 
+
+  # "DELETE", the D in CRUD
+  delete '/articles/:id/delete' do 
+  	@article = Article.find(params[:id])
+  	@article.delete 
+  	erb :delete
   end
 end
